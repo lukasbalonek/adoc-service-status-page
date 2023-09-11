b554f21614dacec4ce52c65bd09202811f958254
Lukáš Balonek <lukas.balonek@fit.cvut.cz>
2023-09-11 11:17:27 +0200

    gen.sh: fix indentations

--- gen.sh
+++ gen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#########################################################################
-#																		#
-#		ASCIIDOCTOR STATUS PAGE GENERATOR (1/2 - gen.sh)				#
-#		BASH script created by Lukas Balonek (2023)						#
-#		<lukas.balonek@gmail.com>										#
-#																		#
-#########################################################################
+#################################################################################
+#                                                               		#
+#		ASCIIDOCTOR STATUS PAGE GENERATOR (1/2 - gen.sh)		#
+#		BASH script created by Lukas Balonek (2023)			#
+#		<lukas.balonek@gmail.com>					#
+#										#
+#################################################################################
 
 ### CONFIGURATION ###
 
@@ -74,73 +74,69 @@ for host_group in $(ls -t config/host_groups/); do
     # define and show host_group_name (category of host)
     host_group_name=$(cat ${host_group_path}/NAME)
     echo "== ${host_group_name}"
-	
-	# Running throught hosts in host_groups/*
+
+    # Running throught hosts in host_groups/*
     for curr_host in ${host_group_path}/hosts/*; do
-	
-	  # Load host configuration
-	  source ${curr_host}
-	
-	  # Show HOST's hostname
+
+      # Load host configuration
+      source ${curr_host}
+
+      # Show HOST's hostname
       if [[ -n ${HOST} ]]; then
-	    echo
-	    if [[ -n ${HOST_PRETTY_NAME} ]]; then
+        echo
+        if [[ -n ${HOST_PRETTY_NAME} ]]; then
           echo "=== ${HOST_PRETTY_NAME}"
-		else
+        else
           echo "=== ${HOST}"
-		fi
+	fi
       fi
 
       # Loop throught collected data
       for curr_data in $(ls -tr ${DATA_DIR}/${HOST}*); do
-        
+
 		# load current data
 		source ${curr_data}
 
-		####################
-		
 		# First of all, we assume it will pass (lmao)
 		HOST_CHECK="PASSED"
-		
+
 		# Make an error if any check fail
-        if [[ ${PING_CHECK} = "FAILED" ]]; then HOST_CHECK="FAILED"; fi
-        if [[ ${HTTP_CHECK} = "FAILED" ]]; then HOST_CHECK="FAILED"; fi
+	        if [[ ${PING_CHECK} = "FAILED" ]]; then HOST_CHECK="FAILED"; fi
+        	if [[ ${HTTP_CHECK} = "FAILED" ]]; then HOST_CHECK="FAILED"; fi
 		if [[ ${HTTPS_CHECK} = "FAILED" ]]; then HOST_CHECK="FAILED"; fi
-		
-		####################
-		
+
 		# result file
 		RESULT_FILE_PATH="$(basename ${RESULTS_DIR})/$(basename ${curr_data}).html"
 		# Replace "#DEL_ME" with "nothing"
-        sed -i "s/^<p>#DEL_ME/<p>/g" "${TARGET_DIR}/${RESULT_FILE_PATH}"
+	        sed -i "s/^<p>#DEL_ME/<p>/g" "${TARGET_DIR}/${RESULT_FILE_PATH}"
 
-        # Display bar after information supplied
-        if [[ ${HOST_CHECK} = "PASSED" ]]; then
-          echo -n "${BAR_OK}[${MEDIA_BAR_DEFAULT_OPTS}, link=${RESULT_FILE_PATH}, title=${TIMESTAMP}]"
+	        # Display bar after information supplied
+	        if [[ ${HOST_CHECK} = "PASSED" ]]; then
+	          echo -n "${BAR_OK}[${MEDIA_BAR_DEFAULT_OPTS}, link=${RESULT_FILE_PATH}, title=${TIMESTAMP}]"
 		# if some check failed
-        else
-          echo -n "${BAR_FAIL}[${MEDIA_BAR_DEFAULT_OPTS}, link=${RESULT_FILE_PATH}, title=${TIMESTAMP}]"
+	        else
+	          echo -n "${BAR_FAIL}[${MEDIA_BAR_DEFAULT_OPTS}, link=${RESULT_FILE_PATH}, title=${TIMESTAMP}]"
 		fi
 
 		# unset variable that defines any failed check
-        unset HOST_CHECK
-		
+        	unset HOST_CHECK
+
 		# unset loaded variables
 		unset PING_CHECK
 		unset HTTP_CHECK
 		unset HTTPS_CHECK
-		
+
 		unset HOST_PRETTY_NAME
 
         # tactical(formatting) newline
 	    echo
-		
+
       done
 
-	done
+    done
 
   fi
-  
+
 done
 
 } 1> index.adoc
